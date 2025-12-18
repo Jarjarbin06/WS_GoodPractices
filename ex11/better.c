215320f1b8904480dc5aba76354beb39711fc7c5
Nathan Jarjarbin <nathan.amaraggi@epitech.eu>
2025-12-18 11:31:33 +0100

    [ADD] exo11 better.c

---
+++ ex11/better.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define EXIT_SUCCESS 0
+#define EXIT_ERROR 84
+
+char *make_message(const char *name)
+{
+    char *buffer = malloc(sizeof(char) * (strlen("Hello ") + strlen(name) + 1));
+
+    if (!buffer)
+        return NULL;
+    sprintf(buffer, "Hello %s", name);
+    return buffer;
+}
+
+int main()
+{
+    char *message = make_message("world");
+
+    if (!message)
+        return EXIT_ERROR;
+    printf("%s\n", message);
+    free(message);
+    return EXIT_SUCCESS;
+}
+
