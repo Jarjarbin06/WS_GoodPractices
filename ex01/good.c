db2585ab58c70bc9f4dde26a2415f06f56a5cb2e
Nathan Jarjarbin <nathan.amaraggi@epitech.eu>
2025-12-18 10:51:38 +0100

    [ADD] exo1 and exo2 correction & .gitignore

---
+++ ex01/good.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define EXIT_SUCCESS 0
+#define EXIT_ERROR 84
+
+int addition(int a, int b)
+{
+    int result = a + b;
+
+    return result;
+}
+
+int main(void)
+{
+    int x = 5;
+    int y = 7;
+    int r = 0;
+
+    r = addition(x, y);
+    printf("Result: %d\n", r);
+    return EXIT_SUCCESS;
+}
+
---
+++ ex02/good.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define EXIT_SUCCESS 0
+#define EXIT_ERROR 84
+
+int length(char *string)
+{
+    int idx = 0;
+
+    while (string[idx] != '\0')
+        idx++;
+    return idx;
+}
+
+int main()
+{
+    char *message = "hello";
+
+    int len = length(message);
+    printf("len = %d\n", len);
+    return 0;
+}
+
